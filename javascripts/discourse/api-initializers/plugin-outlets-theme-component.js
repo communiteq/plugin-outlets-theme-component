94dca39aba18081641ad00112334595b0032efeb
Richard <richard@communiteq.com>
2024-04-26 15:05:15 +0000

    Admin-only option

--- javascripts/discourse/api-initializers/plugin-outlets-theme-component.js
+++ javascripts/discourse/api-initializers/plugin-outlets-theme-component.js
@@ -2,5 +2,8 @@ import { apiInitializer } from 'discourse/lib/api';
 import PluginOutletsHeaderIcon from "../components/plugin-outlets-header-icon";
 
 export default apiInitializer('0.11.1', (api) => {
-  api.headerIcons.add("plugin-outlets-header-icon", PluginOutletsHeaderIcon);
+  let currentUser = api.getCurrentUser();
+  if ((!settings.admin_only) || currentUser.admin) {
+    api.headerIcons.add("plugin-outlets-header-icon", PluginOutletsHeaderIcon);
+  }
 });
